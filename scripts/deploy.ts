d658e35af1e1203db9709db7200b2b34f5ba5b6b
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-06-30 20:50:31 +0300

    added args for contract to the deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,19 @@
 import { ethers } from "hardhat";
 
-const CONTRACT_NAME="CONTRACT_NAME";
+const CONTRACT_NAME="LpTokenFarming";
+
+const LP_TOKEN_ADDRESS = process.env.LP_TOKEN_ADDRESS;
+const REWARD_TOKEN_ADDRESS = process.env.REWARD_TOKEN_ADDRESS;
+
+const farmingEpoch = process.env.FARMING_EPOCH;
+const rewardPerFarmingEpoch = process.env.REWARD_PER_FARMING_EPOCH;
+const lockEpoch = process.env.LOCK_EPOCH;
 
 async function main() {
     const contractFactory = await ethers.getContractFactory(CONTRACT_NAME);
 
-    const contract = await contractFactory.deploy();
+    const contract = await contractFactory.deploy(LP_TOKEN_ADDRESS, REWARD_TOKEN_ADDRESS,
+                                                                        farmingEpoch, rewardPerFarmingEpoch, lockEpoch);
     await contract.deployed();
     console.log(`${CONTRACT_NAME} deployed to: ${contract.address}`);
 }
